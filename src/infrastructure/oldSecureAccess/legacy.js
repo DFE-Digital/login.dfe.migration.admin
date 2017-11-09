9d9f70ca84593158982273a7889fa05730b40cf3
Simon Bull <simon.bull.84@googlemail.com>
2017-11-09 15:33:38 +0000

    Get users services

--- src/infrastructure/oldSecureAccess/legacy.js
+++ src/infrastructure/oldSecureAccess/legacy.js
@@ -1,32 +1,86 @@
 'use strict';
 
+const { uniqBy } = require('lodash');
 const { Op } = require('sequelize');
-const { users, organisations } = require('./schemas/legacySecureAccess.schema');
+const { users, applications } = require('./schemas/legacySecureAccess.schema');
 
-const mapUserEntity = (user) => {
+const roleMapping = [
+  { osa: 'end_user', nsa: { id: 0, name: 'End user' } },
+  { osa: 'approver', nsa: { id: 10000, name: 'Approver' } },
+];
+
+const mapUserEntity = async (user) => {
+  const userApplications = uniqBy(user.groups.map((group) => {
+    return {
+      id: group.application,
+    };
+  }), (item) => {
+    return item.id;
+  }).filter((application) => application.id !== 1);
+
+  const userRoles = user.groups.filter((group) => group.application == 1)
+    .map((group) => {
+      return roleMapping.find((mapping) => mapping.osa === group.code);
+    }).sort((x, y) => {
+      if (x == null) {
+        return 1;
+      }
+      if (x.nsa.id > y.nsa.id) {
+        return -1;
+      }
+      if (x.nsa.id < y.nsa.id) {
+        return 1;
+      }
+      return 0;
+    });
+
+  const services = (await Promise.all(userApplications.map(async (application) => {
+    const applicationEntity = await applications.find({
+      where: {
+        id: {
+          [Op.eq]: application.id,
+        },
+      },
+    });
+    return {
+      id: application.id,
+      name: applicationEntity.dataValues.name,
+      role: userRoles[0].nsa,
+    };
+  }))).sort((x, y) => {
+    if (x.name < y.name) {
+      return -1;
+    }
+    if (x.name > y.name) {
+      return 1;
+    }
+    return 0;
+  });
+
   return {
     firstName: 'fix',// user.dataValues.first_name,
     lastName: 'this', // user.dataValues.last_name,
     email: user.dataValues.email,
-    username : user.dataValues.username,
+    username: user.dataValues.username,
     organisation: {
       name: user.org.dataValues.name
-    }
+    },
+    services,
   }
-}
+};
 
 const searchForUsers = async (criteria) => {
   try {
     const userEntities = await users.findAll({
       where: {
         [Op.or]: [
-          {username: {[Op.like]: `%${criteria}%`}},
-          {email: {[Op.like]: `%${criteria}%`}},
+          { username: { [Op.like]: `%${criteria}%` } },
+          { email: { [Op.like]: `%${criteria}%` } },
         ]
       },
-      include: ['org']
+      include: ['org', 'groups']
     });
-    return userEntities.map(mapUserEntity);
+    return await Promise.all(userEntities.map(mapUserEntity));
   }
   catch (e) {
     throw e;
@@ -41,7 +95,7 @@ const getUserByUsername = async (username) => {
           [Op.eq]: username,
         },
       },
-      include: ['org']
+      include: ['org', 'groups']
     });
     return mapUserEntity(userEntity);
   }
@@ -54,13 +108,3 @@ module.exports = {
   searchForUsers,
   getUserByUsername,
 };
-
-
-/*
-
-<td><%=user.firstName%> <%=user.lastName.toUpperCase()%></td>
-                <td><%=user.email%></td>
-                <td><%=user.username%></td>
-                <td><%=user.organisation.name%></td>
-                <td><a href="/invite/<%=user.username%>">Invite</a></td>
- */
--- src/infrastructure/oldSecureAccess/schemas/legacySecureAccess.schema.js
+++ src/infrastructure/oldSecureAccess/schemas/legacySecureAccess.schema.js
@@ -47,10 +47,55 @@ const organisations = db.define('organisation', {
   timestamps  : false,
 });
 
+const userToGroupMapping = db.define('safe_user_to_user_group', {
+  safe_user: {
+    type: Sequelize.BIGINT,
+  },
+  user_group: {
+    type: Sequelize.BIGINT,
+  },
+}, {
+  tableName: 'safe_user_to_user_group',
+  timestamps  : false,
+});
+
+const groups = db.define('user_group', {
+  id: {
+    type: Sequelize.BIGINT,
+    primaryKey: true,
+  },
+  code: {
+    type: Sequelize.BIGINT,
+  },
+  application: {
+    type: Sequelize.STRING,
+  },
+}, {
+  tableName: 'user_group',
+  timestamps  : false,
+});
+
+const applications = db.define('customer_application', {
+  id: {
+    type: Sequelize.BIGINT,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+  },
+}, {
+  tableName: 'customer_application',
+  timestamps  : false,
+});
+
 
 users.belongsTo(organisations, { as: 'org', foreignKey: 'organisation' });
+users.belongsToMany(groups, { as: 'groups', through: 'safe_user_to_user_group', foreignKey: 'safe_user', otherKey: 'user_group' });
 
 module.exports = {
   users,
   organisations,
+  userToGroupMapping,
+  groups,
+  applications
 };
